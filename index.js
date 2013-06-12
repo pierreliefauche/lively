91dd6dd760dda56f404c566f4578d4521034f1f3
Pierre-Élie <pierre.elie.fauche@gmail.com>
2013-06-13 01:27:38 +0200

    Second version, different approach (return the http request "on steroids"). Began the command line tool.

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 var lively = require('./lib/lively');
 
-var count = 5;
-
-var callback = function(tweet) {
-  console.log('----->', tweet.text);
-
-  if (!--count) {
-    lively.off('obama', callback);
-  }
+var printTweet = function(tweet) {
+  console.log('\t@' + tweet.user.screen_name + '\n\t' + tweet.text.replace(/\s+/g, ' ') + '\n');
 };
 
-var l = lively.on('obama', callback);
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
 
+var query;
+process.stdin.on('data', function (text) {
+  if (query) {
+    lively(query).removeListener('tweet', printTweet);
+    console.log('Tired of "' + query + '"? I can understand.');
+  }
+
+  query = text.replace('\n', '');
+
+  if (query === 'exit') {
+    console.log('Bye');
+    process.exit();
+  }
+
+  console.log('\nLet’s look for tweets matching "' + query + '"…');
+  lively(query, printTweet, function(error) {
+    console.error('Yikes! What just happened?!', error);
+  });
+});
+
+console.log('Enter your query to get the tweets flowing. (type "exit" to… exit.)');
 
